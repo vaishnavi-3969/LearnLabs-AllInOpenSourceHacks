d3b059fa5615941687ffd3d5ce8805c2dda38e5a
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-03-10 00:55:27 +0530

    adding auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { NotFound } from './pages';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          {isAuthenticated ? (
+            <>
+              <Route path='' />
+            </>
+          ) : (
+            <>
+              <Route path='' />
+            </>
+          )}
+          <Route path='*' element=<NotFound /> exact />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-export default Profile
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
+
+export default Profile;
